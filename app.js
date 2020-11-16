7bf655dea7a2495235a32d74fb1d46ca98b04042
Aikol Tashbolot uulu <imaikol@yahoo.com>
2020-11-15 17:44:27 -0800

    inserted comments

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const expressGraphQL = require('express-graphql');
 const schema = require('./schema/schema');
 
 const app = express();
-app.use('/graphiql', expressGraphQL({
+app.use('/graphiql', expressGraphQL({//to see graphiQL interface
     schema,
-    graphiql:true
+    graphiql:true // only intended to be used in dev environment
 }));
 
 app.listen(4000, ()=>{
--- schema/schema.js
+++ schema/schema.js
@@ -3,22 +3,22 @@ const _ = require('lodash');
 
 const {
     GraphQLObjectType, 
-    GraphQLString, 
+    GraphQLString,
     GraphQLInt,
-    GraphQLSchema
+    GraphQLSchema// takes in the root query and returns the graphQL instance
 }= graphql;
 
-const users = [
+const users = [//hard coded users
     {id:"23", firstName: "Bill", age:20},
     {id:"24", firstName: "Jake", age:21},
     {id:"25", firstName: "Ben", age:22},
 ]
 const UserType = new GraphQLObjectType({
-    name:'User',
+    name:'User',//always going to string, giving a name
     fields:{
-        id:{type:GraphQLString},
-        firstName:{type:GraphQLString},
-        age:{type:GraphQLInt}
+        id:{type:GraphQLString},// type string
+        firstName:{type:GraphQLString},// type string
+        age:{type:GraphQLInt}// type integer
     }
 });
 
@@ -28,7 +28,7 @@ const RootQuery = new GraphQLObjectType({
         user: {
             type:UserType,
             args: {id: {type: GraphQLString}},
-            resolve(parentValue, args){
+            resolve(parentValue, args){//most important function(not ever used/notorius, but args been used)
                 return _.find(users, {id: args.id});
             }
         }
